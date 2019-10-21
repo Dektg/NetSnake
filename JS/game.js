ecd7f588a7071ccef85e15df50526c0a7955dcd7
Dektg <56469979+Dektg@users.noreply.github.com>
2019-10-21 14:40:33 +1000

    Исправил ошибку

--- JS/game.js
+++ JS/game.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById("game");//Переташил canvas в js
 const ctx = canvas.getContext("2d");//Сказал что игра в формате 2d
 
 const ground = new Image();//Добавил констане ground значение изображения
-ground.src = "Image/groung.png";// Залил изображение поля в переменную (добавил ей значение)
+ground.src = "Image/ground.png";// Залил изображение поля в переменную (добавил ей значение)
 
 const food = new Image();//Добавил констане food значение изображения
 food.src = "Image/food.png";// Залил изображение еды в переменную (добавил ей значение)
