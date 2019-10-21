ad1cf17fcc7f710e576b0401933ca8744a0c31c4
Dektg <56469979+Dektg@users.noreply.github.com>
2019-10-21 13:14:59 +1000

    Загрузил картинку в переменную ground

--- JS/game.js
+++ JS/game.js
@@ -1,2 +1,5 @@
 const canvas = document.getElementById("game");//Переташил canvas в js
 const ctx = canvas.getContext("2d");//Сказал что игра в формате 2d
+
+const ground = new Image();//Добавил констане ground значение изображения
+ground.src = "Image/groung.png";// Залил изображение поля в переменную (добавил ей значение)
