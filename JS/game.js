0cb7bdc95e02c0d7fcd3a10a14ed7310b56a462b
Dektg <56469979+Dektg@users.noreply.github.com>
2019-10-21 13:26:28 +1000

    добавил еду для змеи
    сначала я хотел добавить сахар но потом подомул слишком транно поэтому змея будет купать арбузы

--- JS/game.js
+++ JS/game.js
@@ -3,3 +3,6 @@ const ctx = canvas.getContext("2d");//Сказал что игра в форма
 
 const ground = new Image();//Добавил констане ground значение изображения
 ground.src = "Image/groung.png";// Залил изображение поля в переменную (добавил ей значение)
+
+const food = new Image();//Добавил констане food значение изображения
+food.src = "Image/food.png";// Залил изображение еды в переменную (добавил ей значение)
