44fb08166580edd59485d1f1c9a07e049871c7e6
Dektg <56469979+Dektg@users.noreply.github.com>
2019-10-25 10:17:53 +1000

    Исправил все ошибки и добавил столкновение

--- JS/game.js
+++ JS/game.js
@@ -1,32 +1,32 @@
-const canvas = document.getElementById("game");//Переташил canvas в js
-const ctx = canvas.getContext("2d");//Сказал что игра в формате 2d
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
 
-const ground = new Image();//Добавил констане ground значение изображения
-ground.src = "Image/ground.png";// Залил изображение поля в переменную (добавил ей значение)
+const ground = new Image();
+ground.src = "img/ground.png";
 
-const food_Img = new Image();//Добавил констане food значение изображения
-food_Img.src = "Image/food.png";// Залил изображение еды в переменную (добавил ей значение)
+const foodImg = new Image();
+foodImg.src = "img/food.png";
 
-let box = 32; //Сделал переменную которая отвечает за создание одного блока (всего в поле 17*15=255 блока по 32px каждый)
+let box = 32;
 
-let score = 0;//Перемнная хранящая в себе счет
+let score = 0;
 
-let food = {//Счайный спавн еды по x и по y
-  x: Math.floor((Math.random() * 17 + 1)) * box,//Math.floor()округляет до целых; Math.random() случайное число от 0 до 1 (Напривмер: 0.2345678); Мы эту функцию умножаем на 17 (блоков в ширину) и прибавляем 1, что-бы еда спавнилась на поле (мы пропуснаем один блок по ширине и еда никогда не появиться вне поля)
-  y: Math.floor((Math.random() * 15 + 3)) * box//Тут все немного по другому, в одном ряду по вертикали (по y) у нас 15 блоков; и умножаем это все дело на box (это длинна одного блока) то есть на 32px
+let food = {
+	x: Math.floor((Math.random() * 17 + 1)) * box,
+	y: Math.floor((Math.random() * 15 + 3)) * box,
 };
 
-let snake = [];//Состав змейки
-snake [0] = {//Нулевой элемент змейки это её голова
-  x: 9 * box,//Отображаем ровно по центу
-  y: 10 * box//Отображаем ровно по центу
-};//Кривыми скобками указываем что это объект
+let snake = [];
+snake[0] = {
+	x: 9 * box,
+	y: 10 * box
+};
 
-document.addEventListener("keydoun", direction);
+document.addEventListener("keydown", direction);
 
 let dir;
 
-function direction (event) {
+function direction(event) {
 	if(event.keyCode == 37 && dir != "right")
 		dir = "left";
 	else if(event.keyCode == 38 && dir != "down")
@@ -37,37 +37,56 @@ function direction (event) {
 		dir = "down";
 }
 
-function Draw_Game() {
-  ctx.drawImage(ground, 0, 0)//Нарисовал поле (ground) на всю ширену canvas
-
-  ctx.drawImage(food_Img, food.x, food.y);//Арбуз с координатами food
-
-  for (var i = 0; i < snake.length; i++) {// В цикле она будет увеличиваться если съест арбуз
-    ctx.fillStyle = "red";//Цвет змейки
-    ctx.fillRect(snake[i].x, snake[i].y, box, box);//fillRect - создать объект в массиве snake под номером [i] (отчет идет от нуля значит первый объект это голова змейки(snake[0]) которая спавница в центре x: 9 * box, y: 10 * box) a box, box в конче это ширина и высота блока
-  }
-
-  ctx.fillStyle = "#a2d149";//Цвет текста
-  ctx.font = "35px Arial";//Шрифт
-  ctx.fillText(score, box * 2, box * 1.6)//Координаты счетчика
-
-  let Snake_X = snake[0].x;
-  let Snake_Y = snake[0].y;
-
-  snake.pop();//удалить последний элемент
-
-  if (dir == "left") Snake_X -= box;
-  if (dir == "right") Snake_X += box;
-  if (dir == "down") Snake_Y += box;
-  if (dir == "up") Snake_Y -= box;
-
-  let new_Head = {
-    x: Snake_X,
-    y: Snake_Y
-  };
-
-  snake.unshift(new_Head);//Помецаем в самое начало координаты изменённого положения головы (переписываем координаты головы на 1 box в любую сторону)
-
+function eatTail(head, arr) {
+	for(let i = 0; i < arr.length; i++) {
+		if(head.x == arr[i].x && head.y == arr[i].y)
+			clearInterval(game);
+	}
 }
 
-let game = setInterval(Draw_Game, 100);//Функция будет вызываться каждые 100 милисекунд (тоесть обновляться каждые 100 милимекунд)
+function drawGame() {
+	ctx.drawImage(ground, 0, 0);
+
+	ctx.drawImage(foodImg, food.x, food.y);
+
+	for(let i = 0; i < snake.length; i++) {
+		ctx.fillStyle = i == 0 ? "green" : "red";
+		ctx.fillRect(snake[i].x, snake[i].y, box, box);
+	}
+
+	ctx.fillStyle = "white";
+	ctx.font = "50px Arial";
+	ctx.fillText(score, box * 2.5, box * 1.7);
+
+	let snakeX = snake[0].x;
+	let snakeY = snake[0].y;
+
+	if(snakeX == food.x && snakeY == food.y) {
+		score++;
+		food = {
+			x: Math.floor((Math.random() * 17 + 1)) * box,
+			y: Math.floor((Math.random() * 15 + 3)) * box,
+		};
+	} else
+		snake.pop();
+
+	if(snakeX < box || snakeX > box * 17
+		|| snakeY < 3 * box || snakeY > box * 17)
+		clearInterval(game);
+
+	if(dir == "left") snakeX -= box;
+	if(dir == "right") snakeX += box;
+	if(dir == "up") snakeY -= box;
+	if(dir == "down") snakeY += box;
+
+	let newHead = {
+		x: snakeX,
+		y: snakeY
+	};
+
+	eatTail(newHead, snake);
+
+	snake.unshift(newHead);
+}
+
+let game = setInterval(drawGame, 110);
